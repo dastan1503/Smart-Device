e26259807fb1d29361a8b81be62df5eae3c2ba30
Bogdan <dastan_krimea@mail.ru>
2019-08-15 13:13:30 +0300

    JS show\hide modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,57 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+(function () {
+  var KEYCODE_ESC = 27;
+  var KEYCODE_ENTER = 13;
+  var closeButton = document.querySelector('.modal__close');
+  var openButton = document.querySelector('.header__open-modal');
+  var modal = document.querySelector('.modal');
 
-pageHeader.classList.remove('page-header--nojs');
+  // открытие/закрытие модалки
+  var showModal = function () {
+    modal.classList.remove('visually-hidden');
+    closePopup(closeButton, modal);
+  };
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  openButton.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === KEYCODE_ENTER) {
+      showModal();
+    }
+  });
+  openButton.addEventListener('click', function () {
+    showModal();
+  });
+
+  // функция закрытия окна
+  var closePopup = function (button, popup) {
+    var closeWindow = function () {
+      popup.classList.add('visually-hidden');
+      document.removeEventListener('keydown', closePopupEsc);
+    };
+
+    var onEscKeydownHandler = function (evt) {
+      if (evt.keyCode === KEYCODE_ESC) {
+        closeWindow();
+      }
+    };
+
+    var closePopupEsc = function () {
+      document.addEventListener('keydown', onEscKeydownHandler);
+    };
+
+    var closePopupEnter = function () {
+      closeButton.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === KEYCODE_ENTER) {
+          closeWindow();
+        }
+      });
+    };
+    var closeClick = function () {
+      closeButton.addEventListener('click', function () {
+        closeWindow();
+      });
+    };
+    closePopupEsc();
+    closePopupEnter();
+    closeClick();
+  };
+})();
